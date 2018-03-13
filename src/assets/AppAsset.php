8879df62b64aacecd871a38cb88f4175b09d22a2
Viktor <shiryakovv@gmail.com>
2018-03-13 23:32:34 +0700

    1

---
+++ src/assets/AppAsset.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace svsoft\yii\admin\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/svsoft/yii2-admin/assets';
+
+    public $css = [
+        'css/admin.css',
+        'css/presets.css',
+    ];
+    public $js = [
+        //'js/admin.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+        'svsoft\yii\admin\assets\AdminLtePluginAsset',
+    ];
+
+    public function init()
+    {
+        parent::init();
+    }
+}
